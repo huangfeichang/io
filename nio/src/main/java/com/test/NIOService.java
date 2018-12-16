c721eda9d99f278cc468d0ee1a4d75f0e1dacd7b
huangfeichang <2606450008@qq.com>
2018-12-16 12:29:30 +0800

    nio同步非阻塞

--- nio/src/main/java/com/test/NIOService.java
+++ nio/src/main/java/com/test/NIOService.java
@@ -2,6 +2,7 @@ package com.test;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -19,9 +20,11 @@ public class NIOService {
             /*设置非阻塞模式*/
             serverSocketChannel.configureBlocking(false);
 
+            System.err.println("NIO通道启动成功，监听端口：" + StaticUtil.REGISTER_PORT);
+
             /*注册一个选择器*/
-            Selector selector = Selector.open();
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            /*Selector selector = Selector.open();
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);*/
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,9 +35,27 @@ public class NIOService {
         while (serverFlag) {
             try {
                 SocketChannel client = serverSocketChannel.accept();
+                if (client != null) {
+                    System.err.println("------>" + client);
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    int l = client.read(byteBuffer);
+                    while (true) {
+                        if (l > 0) {
+                            byteBuffer.clear();
+                            byteBuffer.flip();
+                            System.err.println("从客户端获取到的数据：" + new String(byteBuffer.array(), "UTF-8"));
+                        }
+
+                    }
+
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new NIOService().serverHandle();
     }
 }
